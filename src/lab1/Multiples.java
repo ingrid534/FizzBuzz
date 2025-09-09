bc955326cbd61a217f230415bb8ab40d46506903
Ingrid <195973715+ingrid534@users.noreply.github.com>
2025-09-09 12:35:06 -0400

    Finished multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,19 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+//        multiples();
+    }
+
+    public static int multiples(int n, int a, int b) {
         int i = 1;
         int fin = 0;
-        while (i < 1000) {
-            if ((i % 3) == 0 || i % 5 == 0) {
+        while (i < n) {
+            if ((i % a) == 0 || i % b == 0) {
                 fin++;
             }
             i++;
         }
         System.out.println(fin);
+        return fin;
     }
 }
